87f723ccb96a7a4a76dd5f0ab88151d179e7ce54
Daniel Molina <daniel.v.molina1@gmail.com>
2024-02-14 22:17:41 -0800

    Finalizing changes to 'Counting_Elements' and adding file to track problems to revisit

--- Hashing/Counting_Elements/Solution.cpp
+++ Hashing/Counting_Elements/Solution.cpp
@@ -25,38 +25,17 @@ class Solution {
 public:
     int countElements(std::vector<int>& arr) {
         std::set<int> registry(arr.begin(), arr.end());
-        std::cout << "Set:    ";
-        print(registry);
-        std::unordered_map<int,int> hash_map(arr.size());
-        for(int i = 0; i < arr.size(); i++) {
-            if(hash_map.find(arr[i]) != hash_map.end()) {
-                hash_map[arr[i]] = hash_map[arr[i]] + 1;
-            } else {
-                hash_map[arr[i]] = 1;
-            }
-        }
-        std::cout << "Hash_Map: ";
-        print(hash_map);
         int ans = 0;
-        int i = 0;
-        for(auto s: registry) {
-            if(hash_map.find(key) != hash_map.end()) {
-                ans = ans + hash_map[key];
-                std::cout << key << ": " << hash_map[key] << ": " << ans << std::endl;
-            }
+        for(auto a: arr) {
+            if(registry.find(a+1) != registry.end())
+                ans++;
         }
         return ans;     
     }
 };
 
 int main() {
-    std::vector<int> arr{1,3,2,3,5,0};
-    std::cout << "Vector: {";
-    for(auto a: arr) {
-        std::cout << a << ", ";
-    }
-    std::cout << "}" << std::endl;
-    
+    std::vector<int> arr{1,3,2};
     Solution s1;
     int ans = s1.countElements(arr);
     std::cout << ans << std::endl;
