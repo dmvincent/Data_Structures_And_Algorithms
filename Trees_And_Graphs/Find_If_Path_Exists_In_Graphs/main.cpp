b20e6c82e625c743851405afe8a271d284ca9610
Daniel Molina <daniel.v.molina1@gmail.com>
2024-04-11 12:21:30 -0700

    Adding solution to 'Find_If_Path_Exists_In_Graphs'

--- Trees_And_Graphs/Find_If_Path_Exists_In_Graphs/main.cpp
+++ Trees_And_Graphs/Find_If_Path_Exists_In_Graphs/main.cpp
@@ -15,35 +15,32 @@ void printHash(std::unordered_map<int, std::set<int>>& hash_map) {
 
 class Solution {
 public:
-  std::set<int> seen;
-  std::unordered_map<int, std::set<int>> hash_map;
+  std::unordered_map<int, std::vector<int>> hash_map;
   
   bool validPath(int n, std::vector<std::vector<int>>& edges, int source, int destination) {
     if(n==1) return true;
+    std::vector<bool> seen(n);
     // Pre-Process Hash Map
     for(auto m: edges) {
-      hash_map[m[0]].insert(m[1]);
-      hash_map[m[1]].insert(m[0]);
+      hash_map[m[0]].push_back(m[1]);
+      hash_map[m[1]].push_back(m[0]);
     }
     
     // Call DFS
-    return dfs(source, destination);
+    return dfs(source, destination, seen);
   }
   
-  bool dfs(int source, int destination) {
-    bool ans = false;
-    if(seen.find(source) == seen.end()) {
-      seen.insert(source);
-      if(hash_map[source].find(destination) != hash_map[source].end())
-        return true;
-      else {
-        for(auto iter = hash_map[source].begin(); iter != hash_map[source].end(); iter++) {
-          source = *iter;
-          return dfs(source, destination);
+  bool dfs(int source, int destination, std::vector<bool>& seen) {
+    if(source == destination) return true;
+    if(!seen[source]) {
+      seen[source] = true;
+        for(auto iter: hash_map[source]) {
+          source = iter;
+          if(dfs(source, destination, seen))
+            return true;
         }
-      }
     }
-    return ans;
+    return false;
   }
 };
 
@@ -51,12 +48,11 @@ int main() {
   //std::vector<std::vector<int>> edges{{0,1},{1,2},{2,0}};
   //int n{3}, source{0}, destination{2};
 
-  std::vector<std::vector<int>> edges = {{0,1},{0,2},{3,5},{5,4},{4,3}};
-  int n{6}, source{0}, destination{5};
+  //std::vector<std::vector<int>> edges = {{0,1},{0,2},{3,5},{5,4},{4,3}};
+  //int n{6}, source{0}, destination{5};
 
-  //std::vector<std::vector<int>> edges = {{0,7},{0,8},{6,1},{2,0},{0,4},{5,8},{4,7},{1,3},{3,5},{6,5}};
-  //int n{10}, source{7}, destination{5};
-
+  std::vector<std::vector<int>> edges = {{0,7},{0,8},{6,1},{2,0},{0,4},{5,8},{4,7},{1,3},{3,5},{6,5}};
+  int n{10}, source{7}, destination{5};
 
   Solution s1;
   bool ans = s1.validPath(n, edges, source, destination);
