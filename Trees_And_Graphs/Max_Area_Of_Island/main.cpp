e4015da9569ff92bf56d795df731ad38863a0264
Daniel Molina <daniel.v.molina1@gmail.com>
2024-04-14 07:11:49 -0700

    Adding updated partial solution to 'Max_Area_Of_Island'

--- Trees_And_Graphs/Max_Area_Of_Island/main.cpp
+++ Trees_And_Graphs/Max_Area_Of_Island/main.cpp
@@ -18,298 +18,35 @@ public:
       // Get the Row and Column count of the input grid
       int row = grid.size();
       int col = grid[0].size();
+      int rowUpper{0}, rowLower{0}, colLeft{0}, colRight{0};
+      rowLower = row-1;
+      colRight = col -1;
       for(int i = 0; i < row; i++) {
         for(int j = 0; j < col; j++) {
           if(grid[i][j]) {
             land++;
-            if((row <= 3 && col < 3) || (row < 3 && col <= 3)) {
-              if(row <= 3 && col < 3) {
-                if(row == 1 && col == 1);
-                else if(row == 2 && col == 1) {
-                  if(i == 0) {
-                    if(grid[i+1][j])
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                  }
-                  else {
-                    if(grid[i-1][j])
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                  }
-                }
-                else if(row == 3 && col == 1) {
-                  if(i == 0) {
-                    if(grid[i+1][j])
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                  }
-                  else if(i == 1) {
-                    if(grid[i-1][j]) {
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                    }
-                    if(grid[i+1][j]) {
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                    }
-                  }
-                  else {
-                    if(grid[i-1][j])
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                  }
-                }
-                else if(row == 2 && col == 2) {
-                  if(i == 0 && j == 0) {
-                    if(grid[i+1][j])
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                  }
-                  else if(i == 1 && j == 0) {
-                    if(grid[i-1][j])
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                  }
-                  else if(i == 0 && j == 1) {
-                    if(grid[i+1][j])
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                    if(grid[i][j-1])
-                      grid_map[nodeCount].push_back(nodeCount - 1);
-                  }
-                  else if(i == 1 && j == 1) {
-                    if(grid[i-1][j])
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                    if(grid[i][j-1])
-                      grid_map[nodeCount].push_back(nodeCount - 1);
-                  }
-                }
-                else if(row == 3 && col == 2) {
-                  std::cout << "Hello" << std::endl;
-                  if(i == 0 && j == 0) {
-                    if(grid[i+1][j])
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                  }
-                  else if(i == 1 && j == 0) {
-                    if(grid[i-1][j])
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                    if(grid[i+1][j])
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                  }
-                  else if(i == 2 && j == 0) {
-                    if(grid[i-1][j])
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                  }
-                  else if(i == 0 && j == 1) {
-                    if(grid[i+1][j])
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                    if(grid[i][j-1])
-                      grid_map[nodeCount].push_back(nodeCount - 1);
-                  }
-                  else if(i == 1 && j == 1) {
-                    if(grid[i-1][j])
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                    if(grid[i][j-1])
-                      grid_map[nodeCount].push_back(nodeCount -1);
-                    if(grid[i+1][j])
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                  }
-                  else if(i == 2 && j == 1) {
-                    if(grid[i-1][j])
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                    if(grid[i][j-1])
-                      grid_map[nodeCount].push_back(nodeCount - 1);
-                  }
-
-                }
-                else if(row == 1 && col == 2) {
-                  if(i == 0 && j == 0)
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                  else if(i == 0 && j == 1)
-                    if(grid[i][j-1])
-                      grid_map[nodeCount].push_back(nodeCount - 1);
-                }
-              }
-              else {
-                if(row == 1 && col == 3) {
-                  if(i == 0 && j == 0) {
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                  }
-                  else if(i == 0 && j == 1) {
-                    if(grid[i][j - 1])
-                      grid_map[nodeCount].push_back(nodeCount - 1);
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                  }
-                  else if(i == 0 && j == 2) {
-                    if(grid[i][j-1])
-                      grid_map[nodeCount].push_back(nodeCount - 1);
-
-                  }
-                }
-                else if(row == 2 && col == 3) {
-                  if(i == 0 && j == 0) {
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                    if(grid[i+1][j])
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                  }
-                  else if(i == 0 && j == 1) {
-                    if(grid[i][j - 1])
-                      grid_map[nodeCount].push_back(nodeCount - 1);
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                    if(grid[i+1][j])
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                  }
-                  else if(i == 0 && j == 2) {
-                    if(grid[i][j-1])
-                      grid_map[nodeCount].push_back(nodeCount - 1);
-                    if(grid[i+1][j])
-                      grid_map[nodeCount].push_back(nodeCount + col);
-                  }
-                  else if(i == 1 && j == 0) {
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                    if(grid[i-1][j])
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                  }
-                  else if(i == 1 && j == 1) {
-                    if(grid[i][j - 1])
-                      grid_map[nodeCount].push_back(nodeCount - 1);
-                    if(grid[i][j+1])
-                      grid_map[nodeCount].push_back(nodeCount + 1);
-                    if(grid[i-1][j])
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                  }
-                  else if(i == 1 && j == 2) {
-                    if(grid[i][j-1])
-                      grid_map[nodeCount].push_back(nodeCount - 1);
-                    if(grid[i-1][j])
-                      grid_map[nodeCount].push_back(nodeCount - col);
-                  }
-                }
-              }
+            // Check Upper
+            if((i-1) >= rowUpper) {
+              if(grid[i-1][j])
+                grid_map[nodeCount].push_back(nodeCount - col);
             }
-            else {
-              // Define which nodes are Up, Left, Down Right
-              // Create base case for checking all non-border nodes
-              if(i > 0 && i < (row-1) && j > 0 && j < (col-1)) {
-                // Check Up
-                if(grid[i-1][j]) 
-                  grid_map[nodeCount].push_back(nodeCount - col);
-                // Check Left
-                if(grid[i][j-1])
-                  grid_map[nodeCount].push_back(nodeCount - 1);
-                // Check Down
-                if(grid[i+1][j])
-                  grid_map[nodeCount].push_back(nodeCount + col);
-                // Check Right
-                if(grid[i][j+1])
-                  grid_map[nodeCount].push_back(nodeCount + 1);
-              }
+            // Check Left
+            if((j-1) >= colLeft) {
+              if(grid[i][j-1])
+                grid_map[nodeCount].push_back(nodeCount - 1);
 
-              // Check Non-Corner Nodes on Top Row
-              else if(i == 0 && j > 0 && j < (col-1)) {
-                // No upper row exists
-                // Check Left
-                if(grid[i][j-1])
-                  grid_map[nodeCount].push_back(nodeCount - 1);
-                // Check Down
-                if(grid[i+1][j])
-                  grid_map[nodeCount].push_back(nodeCount + col);
-                // Check Right
-                if(grid[i][j+1])
-                  grid_map[nodeCount].push_back(nodeCount + 1);
-              }
-              // Check Non-Corner Nodes on Bottom Row
-              else if(i == (row - 1) && j > 0 && j < (col-1)) {
-                // Check Up
-                if(grid[i-1][j]) 
-                  grid_map[nodeCount].push_back(nodeCount - col);
-                // Check Left
-                if(grid[i][j-1])
-                  grid_map[nodeCount].push_back(nodeCount - 1);
-                // No Lower Row Exists, Default to 0
-                // Check Right
-                if(grid[i][j+1])
-                  grid_map[nodeCount].push_back(nodeCount + 1);
-              }
-              // Check Non-Corner Nodes on Left Column
-              else if(i > 0 && i < (row-1) && j == 0) {
-                // Check Up
-                if(grid[i-1][j]) 
-                  grid_map[nodeCount].push_back(nodeCount - col);
-                // No Columns to the Left, Default to 0
-                // Check Down
-                if(grid[i+1][j])
-                  grid_map[nodeCount].push_back(nodeCount + col);
-                // Check Right
-                if(grid[i][j+1])
-                  grid_map[nodeCount].push_back(nodeCount + 1);
-                
-              }
-              // Check Non-Corner Nodes on Right Column
-              else if(i > 0 && i < (row-1) && j == (col-1)) {
-                // Check Up
-                if(grid[i-1][j]) 
-                  grid_map[nodeCount].push_back(nodeCount - col);
-                // Check Left
-                if(grid[i][j-1])
-                  grid_map[nodeCount].push_back(nodeCount - 1);
-                // Check Down
-                if(grid[i+1][j])
-                  grid_map[nodeCount].push_back(nodeCount + col);
-                // No Columns to the right exists, default to 0
-              }
-
-              // Check top-left corner node
-              else if(i == 0 && j == 0) {
-                // Upper Row does not exist, defualt to 0
-                // Left element does not exist, default to 0
-                // Check Down
-                if(grid[i+1][j])
-                  grid_map[nodeCount].push_back(nodeCount + col);
-                // Check Right
-                if(grid[i][j+1])
-                  grid_map[nodeCount].push_back(nodeCount + 1);
-              }
-              // Check top-right corner node
-              else if(i == 0 && j == (col - 1)) {
-                // Upper Element does not exist, default to 0
-                // Check Left
-                if(grid[i][j-1])
-                  grid_map[nodeCount].push_back(nodeCount - 1);
-                // Check Down
-                if(grid[i+1][j])
-                  grid_map[nodeCount].push_back(nodeCount + col);
-                // Elements to the right does not exist, dault to 0;
-              }
-              // Check bottom-left corner node
-              else if(i == (row -1) && j == 0) {
-                // Check Up
-                if(grid[i-1][j]) 
-                  grid_map[nodeCount].push_back(nodeCount - col);
-                // Left Element Does not exist, default to 0
-                // Lower element does not exist, default to 0
-                // Check Right
-                if(grid[i][j+1])
-                  grid_map[nodeCount].push_back(nodeCount + 1);
-              }
-              // Check bottom-right corner node
-              else if(i == (row - 1) && j == (col - 1)) {
-                // Check Up
-                if(grid[i-1][j]) 
-                  grid_map[nodeCount].push_back(nodeCount - col);
-                // Check Left
-                if(grid[i][j-1])
-                  grid_map[nodeCount].push_back(nodeCount - 1);
-                // Lower element does not exist, default to 0
-                // Right Element does not exist, default to 0
-              }
+            }
+            
+            // Check Down
+            if((i+1) <= rowLower) {
+              if(grid[i+1][j])
+                grid_map[nodeCount].push_back(nodeCount + col);
+            }
+            
+            // Check Right
+            if((j+1) <= colRight) {
+              if(grid[i][j+1])
+                grid_map[nodeCount].push_back(nodeCount + 1);
             }
           }
           nodeCount++; 
@@ -323,7 +60,7 @@ public:
       int nodeCount{0}, land{0};
       std::unordered_map<int,std::vector<int>> grid_map;
       createHashMap(grid, grid_map, nodeCount, land);
-      printHM(grid_map);
+      //printHM(grid_map);
       //std::cout << "NodeCOunt: " << nodeCount << std::endl;
       //std::cout << "Land: " << land << std::endl;
       if(land == 0) return 0;
@@ -335,7 +72,7 @@ public:
         int source = iter->first;
         if(!seen[source]) {
           int curr_max{0};
-          std::cout << "Looking at: " << source << std::endl;
+          //std::cout << "Looking at: " << source << std::endl;
           dfs(grid_map, seen, iter->first, land, curr_max);
           ans = (curr_max>ans)?curr_max:ans;
         }
