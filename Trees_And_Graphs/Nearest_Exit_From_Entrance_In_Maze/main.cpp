6a668d9b67ae4265b834263afe2fdc65422fe425
Daniel Molina <daniel.v.molina1@gmail.com>
2024-04-18 22:39:52 -0700

    Adding solution to 'Nearest_Exit_From_Entrance_In_Maze'

--- Trees_And_Graphs/Nearest_Exit_From_Entrance_In_Maze/main.cpp
+++ Trees_And_Graphs/Nearest_Exit_From_Entrance_In_Maze/main.cpp
@@ -13,17 +13,23 @@ public:
   
   bool onBorder(std::vector<int>& node, std::vector<int>& entrance, int rowLength, int colLength) {
     if(node[0] == 0 || node[0] == (rowLength-1) || node[1] == 0 || node[1] == (colLength - 1)) {
-      //std::cout << "{" << node[0] << ", " << node[1] << "} True" << std::endl;
       return true;
     }
     else {
-      //std::cout << "{" << node[0] << ", " << node[1] << "} False" << std::endl;
       return false;
     }
   }
 
+  bool isStarting(int row, int col, std::vector<int>& entrance) {
+    std::vector<int> loc{row,col};
+    if(loc == entrance)
+      return true;
+    else
+      return false;
+  }
+
   int nearestExit(std::vector<std::vector<char>>& maze, std::vector<int>& entrance) {
-    int steps{0};      
+    //int steps{0};      
     int rowLength = maze.size();
     int colLength = maze[0].size();
     std::vector<std::vector<int>> directions{{-1,0}, {0,1}, {1,0}, {0,-1}};
@@ -31,24 +37,21 @@ public:
     std::set<std::vector<int>> seen;
 
     // Iterate over each level for every while loop
-    queue.push(entrance);
+    queue.push({entrance[0], entrance[1], 0});
     seen.insert(entrance);
     while(!queue.empty()) {
       std::vector<int> node = queue.front();
       queue.pop();
-      if(onBorder(node, entrance, rowLength, colLength) && node != entrance)
-        return steps;
-      std::cout << "=====================" << std::endl;
+      if(onBorder(node, entrance, rowLength, colLength) &&! isStarting(node[0], node[1], entrance))
+        return node[2];
       for(auto d: directions) {
         int row = node[0] + d[0];
         int col = node[1] + d[1];
         if(validStep(row, col, rowLength, colLength, maze, seen)) {
-          std::cout << "Pushing: {" << row << ", " << col << "}" << std::endl;
-          queue.push({row,col});
+          queue.push({row,col, node[2] + 1});
           seen.insert({row,col});
         }
       }
-      steps++;
     }
     return -1;        
   }
