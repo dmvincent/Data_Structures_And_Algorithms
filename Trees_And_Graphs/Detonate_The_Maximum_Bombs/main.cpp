24e6cf5e625948cfbb38b4627081e7c5b4b92fa4
Daniel Molina <daniel.v.molina1@gmail.com>
2024-04-28 03:07:37 -0700

    Adding slow solution to 'Detonate_The_Maximum_Bombs'

--- Trees_And_Graphs/Detonate_The_Maximum_Bombs/main.cpp
+++ Trees_And_Graphs/Detonate_The_Maximum_Bombs/main.cpp
@@ -12,7 +12,7 @@ public:
     for(int i = 0; i < bombs.size(); i++) {
       for(int j = i+1; j < bombs.size(); j++) {
         double d = sqrt(double(pow((bombs[i][0] - bombs[j][0]), 2) + pow((bombs[i][1] - bombs[j][1]), 2)));
-        std::cout << "Node " << i << " is " << d  << " meters away from " << j << std::endl;
+        //std::cout << "Node " << i << " is " << d  << " meters away from " << j << std::endl;
         if(double(bombs[i][2]) >= d) {
           hash_map[i].push_back(j);
         }
@@ -21,7 +21,7 @@ public:
         }
       }
     }
-    std::cout << "================" << std::endl;
+    //std::cout << "================" << std::endl;
   }
 
   void printHashMap(std::unordered_map<int, std::vector<int>>& hash_map) {
@@ -48,39 +48,41 @@ public:
     std::cout << "=================" << std::endl;
 
     // Perform BFS 
-    std::unordered_map<int,int> chainReaction(bombs.size());
     std::set<int> seen;
+    int size = bombs.size();
+    std::vector<int> chain(bombs.size(), 0);
     int max{INT_MIN}, curr{0};
-    for(auto iter = hash_map.begin(); iter != hash_map.end(); iter++) {
-      curr = dfs(iter->first, hash_map, chainReaction, max, seen);
-      if(curr > max)
-        max = curr;
+    for(int i = 0; i < bombs.size(); i++) {
+      if(seen.find(i) == seen.end()) {
+        seen.insert(i);
+        curr = dfs(i, hash_map, max, seen, chain, size);
+        seen.clear();
+        max = std::max(max, curr);
+      }
+      else {
+        max = std::max(max, chain[i]);
+      }
     }
     
-    for(auto iter = chainReaction.begin(); iter != chainReaction.end(); iter++) {
-      std::cout << iter->first << ": " << iter->second << std::endl;
-    }
-
     return max;      
   }
 
-  int dfs(int node, std::unordered_map<int, std::vector<int>>& hash_map, std::unordered_map<int, int>& chainReaction, int& max, std::set<int>& seen) {
+  int dfs(int node, std::unordered_map<int, std::vector<int>>& hash_map, int& max, std::set<int>& seen, std::vector<int>& chain, int& size) {
     std::cout << "Visiting node " << node << std::endl;
-    int totalBombs = 0;
-    if(seen.find(node) == seen.end()) {
-      seen.insert(node);
-      for(int i = 0; i < hash_map[node].size(); i++) {
-        if(seen.find(hash_map[node][i]) == seen.end()) {
-          if(hash_map.find(hash_map[node][i]) != hash_map.end()) {
-             totalBombs =  1 + dfs(hash_map[node][i], hash_map, chainReaction, max, seen);
-          }
-          } else {
-            totalBombs++;
-          }
+    int curr = 0;
+
+    for(int i = 0; i < hash_map[node].size(); i++) {
+      if(seen.find(hash_map[node][i]) != seen.end()) {
+        //chain[node] += chain[hash_map[node][i]];
+      } else {
+        seen.insert(hash_map[node][i]);
+        curr =  curr + dfs(hash_map[node][i], hash_map, max, seen, chain, size);
       }
     }
+    curr = curr + 1;
+    //chain[node] += curr;
 
-    return 1;
+    return curr;
   }
 };
 
@@ -89,7 +91,8 @@ int main() {
   //std::vector<std::vector<int>> bombs{{2,1,3},{6,1,4}};
   //std::vector<std::vector<int>> bombs{{1,1,5},{10,10,5}};
   //std::vector<std::vector<int>> bombs{{1,2,3},{2,3,1},{3,4,2},{4,5,3},{5,6,4}};
-  std::vector<std::vector<int>> bombs{{868,658,84},{82,386,48},{464,157,11},{422,654,85},{864,418,84},{366,314,72},{955,270,60},{460,98,60},{824,147,38},{580,660,27},{423,102,73},{317,471,74}};
+  std::vector<std::vector<int>> bombs{{56,80,2},{55,9,10},{32,75,2},{87,89,1},{61,94,3},{43,82,9},{17,100,6},{50,6,7},{9,66,7},{98,3,6},{67,50,2},{79,39,5},{92,60,10},{49,9,9},{42,32,10}};
+  //std::vector<std::vector<int>> bombs{{868,658,84},{82,386,48},{464,157,11},{422,654,85},{864,418,84},{366,314,72},{955,270,60},{460,98,60},{824,147,38},{580,660,27},{423,102,73},{317,471,74}};
 
   Solution s1;
   int ans = s1.maximumDetonation(bombs);
