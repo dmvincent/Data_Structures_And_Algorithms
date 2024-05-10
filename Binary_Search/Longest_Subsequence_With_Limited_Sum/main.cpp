cc187a9408861d9726c03362b41f25b155e03af5
Daniel Molina <daniel.v.molina1@gmail.com>
2024-05-09 19:10:25 -0700

    Adding alternative solution to 'Longest_Subsequence_With_Limited_Sum' which modifies the users input 'nums' vector to track the sum rather than creating a seperate vector

--- Binary_Search/Longest_Subsequence_With_Limited_Sum/main.cpp
+++ Binary_Search/Longest_Subsequence_With_Limited_Sum/main.cpp
@@ -4,62 +4,60 @@
 
 class Solution {
 public:
-  void printVector(std::vector<int>& vec) {
-    std::cout << "[";
-    for(int i = 0; i < vec.size(); i++) {
-      if(i != vec.size() - 1)
-        std::cout << vec[i] << ", ";
-      else
-        std::cout << vec[i] << "]" << std::endl << std::endl;
-    }
-  }
+  //void printVector(std::vector<int>& vec) {
+  //  std::cout << "[";
+  //  for(int i = 0; i < vec.size(); i++) {
+  //    if(i != vec.size() - 1)
+  //      std::cout << vec[i] << ", ";
+  //    else
+  //      std::cout << vec[i] << "]" << std::endl << std::endl;
+  //  }
+  //}
   std::vector<int> answerQueries(std::vector<int>& nums, std::vector<int>& queries) {
     std::vector<int> ans(queries.size(), 0);
     
-    // Troubleshooting: Print OG nums
-    std::cout << "Original Nums" << std::endl << "=======================" << std::endl;
-    printVector(nums);
+    //// Troubleshooting: Print OG nums
+    //std::cout << "Original Nums" << std::endl << "=======================" << std::endl;
+    //printVector(nums);
 
     // Sort nums array
     sort(nums.begin(), nums.end());
 
-    // Troubleshooting: Print sorted nums
-    std::cout << "Sorted Nums" << std::endl << "=======================" << std::endl;
-    printVector(nums);
+    //// Troubleshooting: Print sorted nums
+    //std::cout << "Sorted Nums" << std::endl << "=======================" << std::endl;
+    //printVector(nums);
 
     // Create Vector tracking running sums in sorted_nums
-    std::vector<int> runningSortedSums(nums.size(), 0);
     int sum = 0;
-    runningSortedSums[0] = nums[0];
     for(int i = 1; i < nums.size(); i++) {
-      runningSortedSums[i] = runningSortedSums[i-1] + nums[i];
+      nums[i] = nums[i-1] + nums[i];
     }
 
-    // Troubleshooting: Print Running Sums
-    std::cout << "Running Sorted Sums" << std::endl << "=======================" << std::endl;
-    printVector(runningSortedSums);
+    //// Troubleshooting: Print Running Sums
+    //std::cout << "Sums of sorted nums" << std::endl << "=======================" << std::endl;
+    //printVector(nums);
 
     
     //// Troubleshooting: Print the query
     //std::cout << "Query" << std::endl << "=======================" << std::endl;
     //printVector(queries);
 
-    // Find location for maintaining sort in runningSortedSums for each query
-    int max, left, right, mid;
+    // Find location for maintaining sort in sorted nums array for each query
+    int left, right, mid;
     std::vector<int> runningSortedSumsLocation(queries.size(), 0);
     for(auto i = 0; i < queries.size(); i++) {
-      max = queries[i];
+      //max = queries[i];
       left = 0;
-      right = runningSortedSums.size() - 1;
+      right = nums.size() - 1;
       while(left <= right) {
         mid = left + (right - left)/2;
 
-        if(runningSortedSums[mid] == max) {
+        if(nums[mid] == queries[i]) {
           left = mid + 1;
           break;
         }
 
-        if(runningSortedSums[mid] >= max)
+        if(nums[mid] >= queries[i])
           right = mid - 1;
         else
           left = mid + 1;
