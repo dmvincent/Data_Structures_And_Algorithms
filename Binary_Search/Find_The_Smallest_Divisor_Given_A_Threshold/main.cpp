d60997c637b2fe4a64fa252f79c5a4043d266a24
Daniel Molina <daniel.v.molina1@gmail.com>
2024-05-10 23:42:10 -0700

    Adding partial solution to 'Find_The_Smalles_Divisor_Given_A_Threshold'

---
+++ Binary_Search/Find_The_Smallest_Divisor_Given_A_Threshold/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h> 
+
+class Solution {
+public:
+  int smallestDivisor(std::vector<int>& nums, int threshold) {
+    int left = 1;
+    int right = INT_MAX/threshold;
+
+    int cycles = 0;
+    while (left <= right) {
+      int mid = left + (right-left)/2;
+      //std::cout << "Cycle: " << cycles++ << " left: " << left << " right: " << right << " mid: " << mid << std::endl;
+      if(check(nums, threshold, mid)) {
+        right = mid - 1;
+      }
+      else {
+        left = mid + 1;
+      }
+    }
+    //std::cout << "Cycles: " << cycles << std::endl;
+    return left;
+  }
+
+  bool check(std::vector<int>& nums, int threshold, int mid) {
+    int sum = 0;
+    for(auto& i: nums) {
+      sum += ceil((double)i/mid); 
+      //if(mid <= 9) {
+      //  //std::cout << sum << " + ";
+      //}
+    }
+    //std::cout << " Result: " << sum << std::endl;
+    if(sum <= threshold)
+      return true;
+
+    return false;
+  }
+};
+
+int main() {
+  std::vector<int> nums{1,2,5,9};
+  int threshold = 6;
+
+  Solution s1;
+  int ans = s1.smallestDivisor(nums, threshold);
+  std::cout << "Answer: " << ans << std::endl;
+  return 0;
+}
