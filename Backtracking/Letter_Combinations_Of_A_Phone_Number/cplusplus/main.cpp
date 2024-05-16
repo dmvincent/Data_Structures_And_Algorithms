963356206a662bc2c1cb37140bf0f22550ff4667
Daniel Molina <daniel.v.molina1@gmail.com>
2024-05-15 23:13:03 -0700

    Adding fastest c++ solution to 'Letter_Combinations_Of_A_Phone_Number'. Instead of creating a hash_map of letters associated with the key, as soon as the current character is calculated, it is added to the current string and then calls backtrack

--- Backtracking/Letter_Combinations_Of_A_Phone_Number/cplusplus/main.cpp
+++ Backtracking/Letter_Combinations_Of_A_Phone_Number/cplusplus/main.cpp
@@ -6,35 +6,12 @@ public:
   std::vector<std::string> letterCombinations(std::string digits) {
     std::vector<std::string> ans;
     std::string curr = {};
-    std::vector<std::vector<char>> hash_map(digits.size()); 
     if(digits == "")
       return {};
     
-    //// Create Hash Map
-    int limit, j;
-    char currChar;
-    for(int i = 0; i < digits.size(); i++) {
-      int num = (int)(digits[i] - 48);
-      //std::cout << num << ": ";
-      if(num == 7 || num == 9)
-        limit = 4;
-      else
-        limit = 3;
-      j = 0;
-      while(j < limit) {
-        if(num <= 7) 
-          hash_map[i].push_back((char)((num - 2)*3 + j + 97));
-        else
-          hash_map[i].push_back((char)((num - 2)*3 + j + 1 + 97));
-        
-        //// Troubleshoot: Print the characters pertaining to each each digit in digits[]
-        //if(j == limit - 1)
-        //  std::cout << currChar << std::endl;
-        //else
-        //  std::cout << currChar << ", ";
-        j++;
-      }
-    }
+    // Start the backtrackint Function
+    backtrack(digits, curr, ans, 0);
+    
 
     //// Troublshoot: Print Hash Map
     //std::cout << "HashMap" << std::endl << "===================" << std::endl;
@@ -48,26 +25,39 @@ public:
     //  }
     //}
 
-
-    backtrack(digits, curr, ans, 0, hash_map);
-
     return ans;
   }
 
-  void backtrack(std::string& digits, std::string& curr, std::vector<std::string>& ans, int i, std::vector<std::vector<char>>& hash_map) {
-    //std::cout << "Backtracking" << std::endl;
+  void backtrack(std::string& digits, std::string& curr, std::vector<std::string>& ans, int i) {
+    // Base case should return when the size of the string to be added to 2d answer vector reachs
+    // the correct size
     if(curr.size() == digits.size()) {
-      //std::cout << "Pushing curr to ans" << std::endl;
       ans.push_back(curr);
-      //std::cout << "Successfull push to ans" << std::endl;
       return;
     }
 
-    for(int j = 0; j < hash_map[i].size(); j++) {
-      //std::cout << "Pushing '" << hash_map[i][j] << "'" << std::endl;
-      curr.push_back(hash_map[i][j]);
-      backtrack(digits, curr, ans, i+1, hash_map);
-      curr.pop_back();
+    // Add the letters assocatiated with the i'th button to the current string
+    int limit, j;
+    char currChar;
+    for(;i < digits.size(); i++) {
+      int num = (int)(digits[i] - 48);
+      //std::cout << num << ": ";
+      if(num == 7 || num == 9)
+        limit = 4;
+      else
+        limit = 3;
+      j = 0;
+      while(j < limit) {
+        if(num <= 7) 
+          currChar = (char)((num - 2)*3 + j + 97);
+        else
+          currChar = (char)((num - 2)*3 + j + 1 + 97);
+        
+        curr.push_back(currChar);
+        backtrack(digits, curr, ans, i+1);
+        curr.pop_back();
+        j++;
+      }
     }
   }
 };
